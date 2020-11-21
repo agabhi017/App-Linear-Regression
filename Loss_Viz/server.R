8ad450554a1c2651ecb642851be3d8844f5673ae
Abhishek Agarwal <agabhi001@gmail.com>
2020-11-21 23:10:24 +0530

    added changes for sample and population distribution

--- Loss_Viz/server.R
+++ Loss_Viz/server.R
@@ -1,16 +1,38 @@
 library(shiny)
 library(ggplot2)
-
+set.seed(2156)
 
 shinyServer(function(input, output) {
 
-    output$Plot <- renderPlot({
-
-        x = seq(1, 100)
-        y0 = input$intercept + input$slope * x
-        y = input$intercept + input$slope * x + rnorm(100, input$mean, input$std)
-        ggplot(NULL, aes(x = x, y = c(y0, y))) + geom_point(size = 2) 
+        
+    x_pop = reactive({
+        x = runif(input$population, 0, 5)
 
     })
+    y_pop = reactive({
+        y = input$intercept + input$slope * x_pop() + rnorm(input$population, input$mean, input$std)
+
+    })
+    
+    x_sam = reactive({
+        x = sample(x_pop(), input$sample_size)
+    })
+    
+    y_sam = reactive({
+        ypop = y_pop()
+        y = ypop[match(x_sam(), x_pop())]
+    })
+    
+    output$Plot_pop = renderPlot({
+
+        ggplot(NULL, aes(x = x_pop(), y =  y_pop())) + geom_point(size = 2) 
+
+    })
+    output$Plot_sample = renderPlot({
+
+        ggplot(NULL, aes(x = x_sam(), y =  y_sam())) + geom_point(size = 2) 
+        
+    })
+    
 
 })
--- Loss_Viz/ui.R
+++ Loss_Viz/ui.R
@@ -4,24 +4,30 @@ library(shiny)
 shinyUI(fluidPage(
 
 
-    titlePanel("Visualizing the impact of different parametes on linear regression"),
+    titlePanel("Visualizing the impact of different parameters on linear regression"),
     br(),
 
     fluidRow(
         column(3,
         wellPanel(
-            h3("Let's prepare some data first"),
+            strong(h3("Let's prepare some data first")),
             br(),
-            h4("For the line Y = A + B*X, enter the intercept 'A' and slope 'B'"),
+            em(h4("For y = a + b*x, enter the intercept 'a' and slope 'b'")),
             splitLayout(
                 numericInput("slope", "Slope", 2), 
                 numericInput("intercept", "Intercept", 2)),
             splitLayout(
                 numericInput("std", "Std of epsilon", 1),
-                numericInput("mean", "Mean of epsilon", 0))
+                numericInput("mean", "Mean of epsilon", 0)),
+            splitLayout(
+                numericInput("population", "Population Size", 1000), 
+                numericInput("sample_size", "Sample Size", 100))
                 )
             ),
-        column(4, 
-               plotOutput('Plot'))
+            
+        column(3, 
+               plotOutput('Plot_pop')),
+        column(3, 
+               plotOutput('Plot_sample'))
         )
 ))
